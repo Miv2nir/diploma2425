81a49018a587b77ac02a7e7c4da5ec9b68f3f12c
miv2nir <miv2nir@gmail.com>
2025-04-03 12:20:42 +0000

    remove login labels + adjust padding

--- timesite/backend/forms.py
+++ timesite/backend/forms.py
@@ -4,10 +4,10 @@ from backend import models
 #TODO: refactor for some fancy new css that doesn't suck
 #essentials
 class RegisterForm(forms.Form):
-    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Email','class': 'login-input-box'}))
-    login = forms.CharField(label='Login', widget=forms.TextInput(attrs={'placeholder': 'Login','class': 'login-input-box'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password','class': 'login-input-box'}))
-    password_verify = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password','class': 'login-input-box'})) #, 'class': 'square_login' - cut out as no css work is done at this point
+    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Email','class': 'login-input-box','id':'email_field'}))
+    login = forms.CharField(label='Login', widget=forms.TextInput(attrs={'placeholder': 'Login','class': 'login-input-box','id':'login_field'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password','class': 'login-input-box','id':'password_field'}))
+    password_verify = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password','class': 'login-input-box','id':'verification_field'})) #, 'class': 'square_login' - cut out as no css work is done at this point
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,7 +16,11 @@ class RegisterForm(forms.Form):
 
 class AuthForm(forms.Form):
     # login = forms.CharField(label='Login', max_length=100)
-    login = forms.CharField(label='Login', widget=forms.TextInput(attrs={'placeholder': 'Login', 'class': 'login-input-box'}))
+    login = forms.CharField(label='Login', widget=forms.TextInput(attrs={'placeholder': 'Login', 'class': 'login-input-box','id':'login_field'}))
 
     # password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'login-input-box'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'login-input-box','id':'password_field'}))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, field in self.fields.items():
+            field.label = ""
