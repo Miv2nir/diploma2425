0bf7686943877dd3b1292f997e474c2401cec4a3
miv2nir <miv2nir@gmail.com>
2025-04-09 16:29:21 +0000

    Project and ProjectTag objects

--- timesite/backend/models.py
+++ timesite/backend/models.py
@@ -15,3 +15,16 @@ class UserInfo(models.Model):
 class UserPFP(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     pfp=models.ImageField(null=True,blank=True,upload_to='user_pfps/')
+
+class ProjectTag(models.Model):
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    name=models.CharField(max_length=100)
+    
+  
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid5, editable=False)
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    icon=models.ImageField(null=True,blank=True,upload_to='project_icons/')
+    name=models.CharField(max_length=100)
+    description=models.TextField(null=True,blank=True)
+    tags=models.ManyToManyField(ProjectTag,blank=True)
