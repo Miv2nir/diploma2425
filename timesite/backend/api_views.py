64c9134a4415873486c576c5c7bcdfc50d2dabca
miv2nir <miv2nir@gmail.com>
2025-04-13 16:41:28 +0000

    some notes

--- timesite/backend/api_views.py
+++ timesite/backend/api_views.py
@@ -5,16 +5,24 @@ from rest_framework.parsers import JSONParser
 import backend.models as models
 import backend.serializers as serializers
 
-#@csrf_exempt #TODO: remove this after csrf validation is complete
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import login_required
+
+#TODO: rewrite login_required to dump a 401 if user not authorized
+# the frontend will need to preserve the url and redirect to /login with query being the current logged in page
+
+#@login_required
 def get_project(request,id):
     '''
     test api to retrieve project metadata info
     '''
+    #if not request.user.pk: #None if not logged in, returns a value otherwise
+        
+    #check if exists
     try:
         project = models.Project.objects.get(pk=id)
     except models.Project.DoesNotExist:
         return HttpResponse(status=404)
-    
     if request.method=='GET':
         serializer=serializers.ProjectSerializer(project)
         return JsonResponse(serializer.data)
