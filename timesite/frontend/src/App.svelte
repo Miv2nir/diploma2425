751d9b8d2decd9f8188718129e663e6271aa65fc
miv2nir <miv2nir@gmail.com>
2025-04-14 14:01:57 +0000

    boot user from /edit if not logged in

--- timesite/frontend/src/App.svelte
+++ timesite/frontend/src/App.svelte
@@ -1,3 +1,54 @@
+<script>
+  /*
+  import { onMount } from "svelte";
+  async function getUser() {
+    const url = '/api/user/data/';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log(json);
+      return json;
+    } catch (error) {
+      console.error(error.message);
+      return null;
+    }
+  }
+  */
+ //const response = fetch('/api/user/data/');
+  //var obj;
+  //fetch('/api/user/data/').then(x=>x.json()).then(data=> {obj = data}).then(()=>{console.log(obj)});
+  
+  var userdata_promise;
+  var user_logged_in = false;
+  userdata_promise = fetch('/api/user/data').then(x=>x.json());
+  console.log(userdata_promise);
+  userdata_promise.then(
+    function(value) {
+      console.log(value);
+      if (value.pk == null) {
+        //user not logged in. redirect to the login page
+        window.location.href = '/login?next=/edit/';
+      }
+      else {
+        user_logged_in = true;
+      }
+    },
+    function(error) {console.log(error);}
+  )
+ /*
+ onMount(async function() {
+  const response = await fetch('/api/user/data/');
+  const data = await response.json();
+  console.log(data.username);
+  userdata=data;
+  console.log(userdata.username);
+ })*/
+
+</script>
+{#if user_logged_in}
 <span class="home-main-container">
   <div class="home-container" id="container-side-1">
       <h2>Recent projects</h2>
@@ -23,3 +74,4 @@
       <a href='/logout/' class="fancy-underline">Log Out</a><br>
   </div>
 </span>
+{/if}
