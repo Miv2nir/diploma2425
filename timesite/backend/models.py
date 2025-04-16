296d76866b2542c531c4f1500bbde0738cbf7610
miv2nir <miv2nir@gmail.com>
2025-04-16 19:48:24 +0000

    links prefab + datastore model

--- timesite/backend/models.py
+++ timesite/backend/models.py
@@ -40,3 +40,17 @@ class Project(models.Model):
     def save(self,*args,**kwargs):
         self.name=self.name.replace('/','').replace('?','')
         super(Project,self).save(*args,**kwargs)
+
+class DataFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file=models.FileField(upload_to='data_store/')
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    name=models.CharField(max_length=100)
+    description=models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self,*args,**kwargs):
+        self.name=self.name.replace('/','').replace('?','')
+        super(Project,self).save(*args,**kwargs)
