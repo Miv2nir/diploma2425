4e02e913e2148fd99e8bd1d45a417c9c1af47f9a
miv2nir <miv2nir@gmail.com>
2025-04-16 20:06:46 +0000

    move animator code into a separate script

--- timesite/frontend/src/App.svelte
+++ timesite/frontend/src/App.svelte
@@ -53,57 +53,5 @@
     <MainPanel user={user} proj_obj={proj_obj} />
     <PipelinePanel user={user} />
   </span>
-  <script>
-    //not part of the svelte component, the code below takes care of the transition animations
-    var container_left = document.getElementById("container-side-1");
-    var container_main = document.getElementById("container-main");
-    var container_right = document.getElementById("container-side-2");
-    var animate = false;
-    var shrunk = window.innerWidth < 1000;
-    function addTransition() {
-      container_left.classList.add("transitioning");
-      container_right.classList.add("transitioning");
-      container_main.classList.add("transitioning");
-      console.log("transitions added");
-    }
-    function removeTransition() {
-      container_left.classList.remove("transitioning");
-      container_right.classList.remove("transitioning");
-      container_main.classList.remove("transitioning");
-      animate = false;
-      console.log("transitions removed");
-    }
-    function delayedRemoveTransition() {
-      //console.log('aea');
-      setTimeout(removeTransition, 100);
-    }
-    function hider() {
-      if (window.innerWidth < 1000) {
-        if (animate == false && shrunk == false) {
-          animate = true;
-          shrunk = true;
-          addTransition();
-        }
-        container_left.classList.add("hidden");
-        container_right.classList.add("hidden");
-        container_main.classList.add("full-width");
-      } else {
-        if (animate == false && shrunk == true) {
-          animate = true;
-          shrunk = false;
-          addTransition();
-        }
-        container_left.classList.remove("hidden");
-        container_right.classList.remove("hidden");
-        container_main.classList.remove("full-width");
-      }
-      //window.addEventListener('resize',hider);
-    }
-
-    window.onresize = hider;
-    //addEventListener('animationstart',(addTransition));
-    addEventListener("transitionend", delayedRemoveTransition);
-    //fire the function on page load, otherwise it doesnt do anything on mobile unless form fields are interacted with
-    hider();
-  </script>
+  <script src="/backend/static/three_panel_animator.js"></script>
 {/if}
