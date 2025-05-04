d5108d1e1e4ee9e7733390cfa4aa1340d84f189f
miv2nir <miv2nir@gmail.com>
2025-05-04 09:37:01 +0000

    add exit button to editor

--- timesite/frontend/src/elements/ProjectThumb.svelte
+++ timesite/frontend/src/elements/ProjectThumb.svelte
@@ -9,7 +9,7 @@
     }
 </script>
 
-<div class="project-item {special_class}">
+<div class="project-item {special_class}" style="position:relative;">
     {#if project.icon}
     <img src="{project.icon}" class="thumb-icon" alt='icon'>
     {:else}
@@ -19,4 +19,10 @@
         <h3 style="margin-bottom: 0; margin-top: 0;">{project.name}</h3>
         By {user.display_name}
     </div>
+    {#if is_special}
+    <!--p style="position:absolute; right:5%;">Exit</p-->
+    <a href="/projects/{project.id}/" style="position:absolute;right:2%; max-width:10rem;">
+        <button type="button" class="login-button-secondary" style="width: 10rem;">Exit</button>
+    </a>
+    {/if}
 </div>
