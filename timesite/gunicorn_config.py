9b7f76217d8a085508b2094d7e70206b7cd4e9c9
miv2nir <miv2nir@gmail.com>
2025-06-08 17:03:53 +0000

    fix wide icons being uncropped + gunicorn tweaks

--- timesite/gunicorn_config.py
+++ timesite/gunicorn_config.py
@@ -1,8 +1,8 @@
 import multiprocessing
 
 bind = "0.0.0.0:8000"  # Replace with your desired IP and port
-worker_class = 'gevent'
-workers = 1
+#worker_class = 'gevent'
+workers = 3
 worker_class = "gthread"
 threads = 2
 timeout = 60
